4105715f504807a5828e2015f8212276ede5e4de
unknown <pedromartes.apps@gmail.com>
2025-10-06 10:12:56 -0300

    arrumando o prisma

--- API/prisma/migrations/20251006125352_init/migration.sql
+++ API/prisma/migrations/20251006125352_init/migration.sql
@@ -88,8 +88,10 @@ CREATE TABLE "public"."Maintainer" (
 CREATE TABLE "public"."Task" (
     "id" SERIAL NOT NULL,
     "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
     "inspectorId" INTEGER NOT NULL,
     "machineId" INTEGER,
+    "expirationDate" TIMESTAMP(3) NOT NULL,
     "createDate" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updateDate" TIMESTAMP(3) NOT NULL,
 
@@ -102,6 +104,7 @@ CREATE TABLE "public"."Machine" (
     "name" TEXT NOT NULL,
     "description" TEXT NOT NULL,
     "location" TEXT NOT NULL,
+    "qrCode" TEXT,
     "createDate" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updateDate" TIMESTAMP(3) NOT NULL,
 
@@ -130,6 +133,14 @@ CREATE TABLE "public"."Subsets" (
 );
 
 -- CreateTable
+CREATE TABLE "public"."_SetsOnMachine" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL,
+
+    CONSTRAINT "_SetsOnMachine_AB_pkey" PRIMARY KEY ("A","B")
+);
+
+-- CreateTable
 CREATE TABLE "public"."_SetToSubsets" (
     "A" INTEGER NOT NULL,
     "B" INTEGER NOT NULL,
@@ -151,6 +162,9 @@ CREATE UNIQUE INDEX "Employees_email_key" ON "public"."Employees"("email");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "TeamMember_teamId_personId_key" ON "public"."TeamMember"("teamId", "personId");
+
+-- CreateIndex
+CREATE INDEX "_SetsOnMachine_B_index" ON "public"."_SetsOnMachine"("B");
 
 -- CreateIndex
 CREATE INDEX "_SetToSubsets_B_index" ON "public"."_SetToSubsets"("B");
@@ -177,7 +191,10 @@ ALTER TABLE "public"."Task" ADD CONSTRAINT "Task_inspectorId_fkey" FOREIGN KEY (
 ALTER TABLE "public"."Task" ADD CONSTRAINT "Task_machineId_fkey" FOREIGN KEY ("machineId") REFERENCES "public"."Machine"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "public"."Sets" ADD CONSTRAINT "Sets_machineId_fkey" FOREIGN KEY ("machineId") REFERENCES "public"."Machine"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "public"."_SetsOnMachine" ADD CONSTRAINT "_SetsOnMachine_A_fkey" FOREIGN KEY ("A") REFERENCES "public"."Machine"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."_SetsOnMachine" ADD CONSTRAINT "_SetsOnMachine_B_fkey" FOREIGN KEY ("B") REFERENCES "public"."Sets"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "public"."_SetToSubsets" ADD CONSTRAINT "_SetToSubsets_A_fkey" FOREIGN KEY ("A") REFERENCES "public"."Sets"("id") ON DELETE CASCADE ON UPDATE CASCADE;
