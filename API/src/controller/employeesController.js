1c8cc5466805f4613a36449aa6a90e3c919678e8
unknown <pedromartes.apps@gmail.com>
2025-10-16 09:50:11 -0300

    atualizando o cadastro dos employees

--- API/src/controller/employeesController.js
+++ API/src/controller/employeesController.js
@@ -5,108 +5,178 @@ const bcrypt = require('bcrypt');
 const employeesController = {
 
     //Create a new employee acount
-    create: async (req, res) => {
-
+    preRegister: async (req, res) => {
         try {
-            const { name, cpf, email, phone, birthDate, password, role } = req.body;
+            const { name, email, role } = req.body;
 
-            if (!name || !cpf || !email || !phone || !birthDate || !password || !role) {
+            if (!name || !email || !role) {
                 return res.status(400).json({
-                    msg: "All the fields are necessary"
-                });
-            };
-
-            const hashedPassword = await bcrypt.hash(password, 10);
-
-            const employeeCreated = await prisma.employees.create({
-                data: {
-                    name, cpf, email, phone, birthDate: new Date(birthDate), password: hashedPassword, role
-                }
-            });
-
-            if (employeeCreated.role == "INSPECTOR") {
-                const inspectorCreates = await prisma.inspector.create({
-                    data: {
-                        id: employeeCreated.id // O campo correto é 'id', não 'employeeId'
-                    }
-                });
-            } else if (employeeCreated.role == "MAINTAINER") {
-                const maintainerCreates = await prisma.maintainer.create({
-                    data: {
-                        id: employeeCreated.id // O campo correto é 'id', não 'employeeId'
-                    }
-                });
-            } else {
-                return res.status(400).json({
-                    msg: "Role must be either INSPECTOR or MAINTAINER"
+                    msg: "Name, email and role are required"
                 });
             }
 
+            const employeeCreated = await prisma.employees.create({
+                data: {
+                    name,
+                    email,
+                    role,
+                    status: "PENDING_SETUP"
+                }
+            })
+
+            if (employeeCreated.role == "INSPECTOR") {
+
+                const inspectorCreates = await prisma.inspector.create({
+
+                    data: {
+
+                        id: employeeCreated.id // O campo correto é 'id', não 'employeeId'
+
+                    }
+
+                });
+
+            } else if (employeeCreated.role == "MAINTAINER") {
+
+                const maintainerCreates = await prisma.maintainer.create({
+
+                    data: {
+
+                        id: employeeCreated.id // O campo correto é 'id', não 'employeeId'
+
+                    }
+
+                });
+
+            } else {
+
+                return res.status(400).json({
+
+                    msg: "Role must be either INSPECTOR or MAINTAINER"
+
+                });
+
+            }
+
+
+
             return res.status(201).json({
+
                 msg: "Employee created successfully",
+
                 id: employeeCreated.id
+
             });
 
         } catch (error) {
 
-            if (error.code === 'P2002') {
-                return res.status(400).json({
-                    msg: "An employee with this data already exists"
-                });
-            }
-
-            console.log(error);
-
+            console.log(error)
             return res.status(500).json({
-                msg: "Internal server error"
-            });
+                msg: "Internal server error",
+                error: error.message
+            })
         }
     },
 
     login: async (req, res) => {
-        const { cpf, password } = req.body;
+        try {
 
-        if (!cpf || !password) {
-            return res.status(400).json({
-                msg: "CPF and password are required"
+            const { email, password } = req.body;
+
+            if (!email || !password) {
+                return res.status(400).json({
+                    msg: "CPF and password are required"
+                });
+            }
+
+            const employeeFind = await prisma.employees.findUnique({
+                where: { email }
             });
-        }
 
-        const employeeFind = await prisma.employees.findUnique({
-            where: { cpf }
-        });
+            if (!employeeFind) {
+                return res.status(404).json({
+                    msg: "Employee not found"
+                });
+            }
 
-        if (!employeeFind) {
-            return res.status(404).json({
-                msg: "Employee not found"
+            const passwordMatch = await bcrypt.compare(password, employeeFind.password);
+
+            if (!passwordMatch) {
+                return res.status(401).json({
+                    msg: "Invalid password or email"
+                });
+            }
+
+            const payload = {
+                id: employeeFind.id,
+                email: employeeFind.email,
+                name: employeeFind.name,
+                role: employeeFind.role,
+                status: employeeFind.status
+            };
+
+            const token = jwt.sign(payload, "SGNldE5pYW0=", {
+                expiresIn: '1d' // Token expiration time
             });
-        }
 
-        const passwordMatch = await bcrypt.compare(password, employeeFind.password);
-
-        if (!passwordMatch) {
-            return res.status(401).json({
-                msg: "Invalid password or CPF"
+            return res.status(200).json({
+                token,
+                user: {
+                    id: employeeFind.id,
+                    name: employeeFind.name,
+                    status: employeeFind.status
+                },
+                id: employeeFind.id,
+                msg: "Employee successfully authenticated"
             });
+
+        } catch (error) {
+
+            console.log(error)
+            return res.status(500).json({
+                msg: "Internal server error",
+                error: error.message
+            })
         }
+    },
 
-        const payload = {
-            id: employeeFind.id,
-            cpf: employeeFind.cpf,
-            email: employeeFind.email,
-            name: employeeFind.name,
-            role: employeeFind.role
-        };
+    completeSetup: async (req, res) => {
+        try {
+            const id = req.user.id;
 
-        const token = jwt.sign(payload, "SGNldE5pYW0=", {
-            expiresIn: '1d' // Token expiration time
-        });
+            const { cpf, phone, birthDate, password } = req.body;
 
-        return res.status(200).json({
-            token,
-            id: employeeFind.id,
-            msg: "Employee successfully authenticated"
-        });
+            if (!cpf || !phone || !birthDate || !password) {
+                return res.status(400).json({
+                    msg: "All fields are necessary"
+                });
+            }
+
+            const hashedPassword = await bcrypt.hash(password, 10);
+
+            await prisma.employees.update({
+                where: { id: Number(id) },
+                data: {
+                    cpf,
+                    phone,
+                    birthDate: new Date(birthDate),
+                    password: hashedPassword,
+                    status: "ACTIVE"
+            })
+
+            return res.status(200).json({
+                msg: "Employee setup completed successfully"
+            });
+
+            
+        } catch (error) {
+            
+            console.log(error)
+            return res.status(500).json({
+                msg: "Internal server error",
+                error: error.message
+            })
+        }
     },
 
     delete: async (req, res) => {
