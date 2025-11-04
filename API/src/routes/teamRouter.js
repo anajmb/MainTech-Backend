93d5731be54fa642741a844166432eb539e9e929
Ana Julia Mathias <estrelaanamb0108@gmail.com>
2025-11-04 15:10:53 -0300

    Adicionando identificar membro

--- API/src/routes/teamRouter.js
+++ API/src/routes/teamRouter.js
@@ -10,6 +10,8 @@ router.get("/get", (req, res) => teamController.getAll(req, res));
 
 router.get("/getUnique/:id", (req, res) => teamController.getUnique(req, res));
 
+router.get("/getByUser/:userId", (req, res) => teamController.getByUser(req, res));
+
 router.put("/update/:id", (req, res) => teamController.update(req, res));
 
 router.delete("/delete/:id", (req, res) => teamController.delete(req, res));
