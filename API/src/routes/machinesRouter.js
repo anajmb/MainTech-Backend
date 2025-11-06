e170db77c12d4255fe678b31fd8d2061c8b81a97
Ana Julia Mathias <estrelaanamb0108@gmail.com>
2025-11-06 10:28:38 -0300

    Arrumando machines router

--- API/src/routes/machinesRouter.js
+++ API/src/routes/machinesRouter.js
@@ -5,15 +5,13 @@ const router = Router();
 
 router.post('/create', (req, res) => machinesController.create(req, res));
 
-router.post('/updateTemperature/:id/temperature', updateTemperature);
+router.post('/updateTemperature/:id/temperature', (req, res) => machinesController.updateTemperature(req, res));
 
 router.get('/get', (req, res) => machinesController.getAll(req, res));
 
 router.get('/getUnique/:id', (req, res) => machinesController.getUnique(req, res));
 
 router.put('/update/:id', (req, res) => machinesController.update(req, res));
-
-router.patch('/updateTemp/:id', (req, res) => machinesController.updateTemperature(req, res));
 
 
 router.delete('/delete/:id', (req, res) => machinesController.delete(req, res));
